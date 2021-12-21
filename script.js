dca7e54a093d709e0f3faa8f3a5b61c636a94049
sirben181 <bensonkamauben@gmail.com>
2021-12-21 11:56:10 +0300

    some content  added for show navigation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const navToggle=document.querySelector(".nav-toggle");
 const links=document.querySelector(".nav-items");
 navToggle.addEventListener("click",function(){
-   console.log(links.classList.toggle('show-links'))
+   links.classList.toggle('show-links')
     // if(links.classList.contains("show-links")){
     //     links.classList.remove('show-links')
     // } else{links.classList.add('show-links')}
